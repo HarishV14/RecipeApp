83f42d44a71b5e4ed27ad4c5c1e59b6633ab7869
HarishV14 <harishv@testpress.in>
2024-10-16 17:00:10 +0530

    Added ListView and DetailView

--- RecipeProject/settings.py
+++ RecipeProject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "RecipeProject.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
